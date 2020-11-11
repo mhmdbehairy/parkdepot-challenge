eeeb1749371ce17e813055a0fe9fc80982f9143a
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-11-12 00:54:27 +0200

    feature: dockerize api, app, and db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ export const App: React.FC<Props> = () => {
   });
 
   useEffect(() => {
-    fetch('http://localhost:4000/refresh_token', {
+    fetch(`http://${process.env.REACT_APP_API}:4000/refresh_token`, {
       method: 'POST',
       credentials: 'include',
     }).then(async (x) => {
